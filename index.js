96a8f19204e284d34877442bd9cb61811093f1d0
Tyler Holloway <tiloooor@Tylers-MacBook-Pro.local>
2019-10-31 14:19:25 -0400

    Configure Google authentication

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-let express = require('express')
-let fs = require('fs')
-let http = require('http')
+const express = require('express');
+const config = require('config');
+const CLIENT_ID = config.get('CLIENT_ID');
+const CLIENT_SECRET = config.get('CLIENT_SECRET');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
-let app = express()
-const port = 8000
-app.listen(port)
+const app = express();
 
-app.set('view engine', 'ejs')
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/assets', express.static('assets'))
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      done(null, profile);
+    }
+  )
+);
 
-app.get('/', function(request, response){
-	response.render('home');
-})
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google'),
+  (req, res) => {
+    res.redirect('/api/home');
+  }
+);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('assets'));
