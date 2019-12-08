06ee0e867fd02b56389062a1bf8bacb873ec9802
Aaron Wade <aaron.wade@yale.edu>
2019-12-08 14:12:54 -0500

    Added call to OpenFema API (used to populate home page) to api/home route

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 
 const app = express();
 
+const OPENFEMA_API_URL = "/api/open";
+
+const FEMAParams = {
+  format: "json",
+  filter: ""
+};
+
 // Enable CORS
 app.use(cors({}));
 
@@ -13,6 +20,21 @@ app.get('/', (req, res) => res.send('API Running...'));
 
 app.get('/api/home', (req, res) => {
   res.render('home');
+  
+  var d = new Date();
+
+  dateString = ('0' + (MyDate.getMonth() + 1)).slice(-2)
+             + ('0' + MyDate.getDate()).slice(-2);
+
+  FEMAParams[filter] = "endswith(declarationDate,'" + dateString + "')";
+
+  let fetchUrl = new URL(OPENFEMA_API_URL);
+  Object.keys(FEMAParams).forEach(key => fetchUrl.searchParams.append(key, FEMAParams[key]));
+
+  const fetched = await fetch(fetchUrl.toString());
+  const data = await fetched.json();
+
+  return res.json(data);
 });
 
 const port = process.env.PORT || 8000;
