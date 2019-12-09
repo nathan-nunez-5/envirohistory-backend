3e4ee8a39126ef317c7af01bd99c660f1d399989
Aaron Wade <aaron.wade@yale.edu>
2019-12-09 13:28:23 -0500

    Moved OpenFEMA API call from index.js to search.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const cors = require('cors');
 
 const app = express();
 
-const OPENFEMA_API_URL = "/api/open";
-
-const FEMAParams = {
-  format: "json",
-  filter: ""
-};
-
 // Enable CORS
 app.use(cors({}));
 
@@ -17,25 +10,6 @@ app.use(cors({}));
 app.use('/api/search', require('./routes/api/search'));
 
 app.get('/', (req, res) => res.send('API Running...'));
-
-app.get('/api/home', (req, res) => {
-  res.render('home');
-  
-  var d = new Date();
-
-  dateString = ('0' + (MyDate.getMonth() + 1)).slice(-2)
-             + ('0' + MyDate.getDate()).slice(-2);
-
-  FEMAParams[filter] = "endswith(declarationDate,'" + dateString + "')";
-
-  let fetchUrl = new URL(OPENFEMA_API_URL);
-  Object.keys(FEMAParams).forEach(key => fetchUrl.searchParams.append(key, FEMAParams[key]));
-
-  const fetched = await fetch(fetchUrl.toString());
-  const data = await fetched.json();
-
-  return res.json(data);
-});
 
 const port = process.env.PORT || 8000;
 
--- routes/api/search.js
+++ routes/api/search.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const GOOGLE_CUSTOM_SEARCH_API_URL = "https://www.googleapis.com/customsearch/v1?";
 const GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyB09RX3f33e5qFL0tjjptIXLlI2VOjv_Lc";
 const CUSTOM_SEARCH_ENGINE_ID = "012399205167094491174:bylu6wpfbyk";
+const OPENFEMA_API_URL = "/api/open";
 
 const searchParams = {
   key: GOOGLE_CUSTOM_SEARCH_API_KEY,
@@ -11,9 +12,14 @@ const searchParams = {
   q: ""
 };
 
+const FEMAParams = {
+  format: "json",
+  filter: ""
+};
+
 // @route   POST api/search
 // @desc    Create a search
-// @access  Private
+// @access  Public
 router.post('/', async (req, res) => {
   console.log('Submitting Search...');
 
@@ -21,6 +27,26 @@ router.post('/', async (req, res) => {
 
   let fetchUrl = new URL(GOOGLE_CUSTOM_SEARCH_API_URL);
   Object.keys(searchParams).forEach(key => fetchUrl.searchParams.append(key, searchParams[key]));
+
+  const fetched = await fetch(fetchUrl.toString());
+  const data = await fetched.json();
+
+  return res.json(data);
+});
+
+// @route   GET api/search/today
+// @desc    Get data associated with today's date
+// @access  Public
+router.get('/today', (req, res) => {  
+  var d = new Date();
+
+  dateString = ('0' + (MyDate.getMonth() + 1)).slice(-2)
+             + ('0' + MyDate.getDate()).slice(-2);
+
+  FEMAParams[filter] = "endswith(declarationDate,'" + dateString + "')";
+
+  let fetchUrl = new URL(OPENFEMA_API_URL);
+  Object.keys(FEMAParams).forEach(key => fetchUrl.searchParams.append(key, FEMAParams[key]));
 
   const fetched = await fetch(fetchUrl.toString());
   const data = await fetched.json();
