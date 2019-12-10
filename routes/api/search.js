2383d4ed19d2a0bf168274dda1ae9471bdc431a1
Aaron Wade <aaron.wade@yale.edu>
2019-12-10 16:49:30 -0500

    Added filtering logic for OpenFEMA API in the backend

--- routes/api/search.js
+++ routes/api/search.js
@@ -7,7 +7,14 @@ const GOOGLE_CUSTOM_SEARCH_API_URL =
   'https://www.googleapis.com/customsearch/v1?';
 const GOOGLE_CUSTOM_SEARCH_API_KEY = 'AIzaSyB09RX3f33e5qFL0tjjptIXLlI2VOjv_Lc';
 const CUSTOM_SEARCH_ENGINE_ID = '012399205167094491174:bylu6wpfbyk';
+
 const OPENFEMA_API_URL = 'https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries';
+const DISASTER_DECLARATIONS_SUMMARIES = "DisasterDeclarationsSummaries";
+const INCIDENT_BEGIN_DATE = "incidentBeginDate";
+const INCIDENT_END_DATE = "incidentEndDate";
+const DECLARATION_DATE = "declarationDate";
+
+const MIN_NUM_RESULTS = 5;
 
 const searchParams = {
   key: GOOGLE_CUSTOM_SEARCH_API_KEY,
@@ -17,7 +24,6 @@ const searchParams = {
 
 const FEMAParams = {
   format: 'json',
-  filter: ''
 };
 
 // @route   POST api/search
@@ -42,20 +48,20 @@ router.post('/', async (req, res) => {
   }
 });
 
+const createDateString = (date) => {
+  let dateString = ('0' + (date.getMonth() + 1)).slice(-2)
+                     + '-'
+                     + ('0' + date.getDate()).slice(-2);
+  return dateString;
+} 
+
 // @route   GET api/search/today
 // @desc    Get data associated with today's date
 // @access  Public
 router.get('/today', async (req, res) => {
   try {
-    var d = new Date();
-
-    dateString =
-      ('0' + (d.getMonth() + 1)).slice(-2) + '-' +
-      ('0' + d.getDate()).slice(-2);
-
-    FEMAParams['filter'] = "substringof('" + dateString + "',declarationDate)";
-
     let fetchUrl = new URL(OPENFEMA_API_URL);
+
     Object.keys(FEMAParams).forEach((key) =>
       fetchUrl.searchParams.append(key, FEMAParams[key])
     );
@@ -63,7 +69,25 @@ router.get('/today', async (req, res) => {
     const fetched = await fetch(fetchUrl.toString());
     const data = await fetched.json();
 
-    return res.json(data);
+    let d = new Date();
+    let dateString = createDateString(d);
+
+    let filteredData = [];
+
+    while (filteredData.length < MIN_NUM_RESULTS) {
+      for (const entry of data[DISASTER_DECLARATIONS_SUMMARIES]) {
+        if (entry[INCIDENT_BEGIN_DATE].includes(dateString)
+            || entry[INCIDENT_END_DATE].includes(dateString)
+            || entry[DECLARATION_DATE].includes(dateString)) {
+          filteredData.push(entry);
+        }
+      }
+
+      d.setDate(d.getDate() + 1);
+      dateString = createDateString(d);
+    }
+
+    return res.json(filteredData);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
