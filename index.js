fad3a8707c62d778c3f505da4b2ffee4fb879a22
Tyler Holloway <tiloooor@Tylers-MacBook-Pro.local>
2019-12-11 19:10:40 -0500

    Removed res.send

--- index.js
+++ index.js
@@ -21,12 +21,13 @@ app.use(function(req, res, next) {
 // Routes
 app.use('/api/search', require('./routes/api/search'));
 
-app.get('/', (req, res) => res.send('API Running...'));
+// app.get('/', (req, res) => res.send('API Running...'));
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('envirohistory-frontend/build'));
 
   app.get('*', (req, res) => {
+    console.log('in here..');
     res.sendFile(
       path.resolve(__dirname, 'envirohistory-frontend', 'build', 'index.html')
     );
